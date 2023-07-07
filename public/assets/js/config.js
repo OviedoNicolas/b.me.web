ae61e5d98e4276554d0f646ff2414b8fb9c6c032
Nicolas Oviedo <nicoohdeoviedo@gmail.com>
2023-07-07 18:27:52 -0300

    se terminaron estilos y se comenzo logica del paso 5

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,3 +1,5 @@
+const { application } = require("express");
+
 let mainUrlApi = "http://localhost:3002/";
 let mainData = []
 
@@ -25,11 +27,13 @@ let mainData = []
 //     };
 // };
 
+
+// Obtiene los valores de los inputs Name y Url
 const nameInput = document.getElementById('name');
 const urlInput = document.getElementById('url');
 
 let valorDefecto = '';
-
+// Completa el input Ulr con beauting.me/ seguido del nombre del negocio
 nameInput.addEventListener('input', () => {
     const nameValue = nameInput.value.trim();
     const nameTienda = nameValue.replace(/\s+/g, ''); 
@@ -40,13 +44,13 @@ nameInput.addEventListener('input', () => {
         urlInput.value = '';
     }
 });
-
+// En caso que el nombre este vacio, devuelva al input Url el valor por defecto
 nameInput.addEventListener('blur', () => {
     if (nameInput.value === '') {
         nameInput.value = valorDefecto;
     }
 });
-
+// Obtiene los datos de los inputs del paso 1 y los envia a la api para crear los datos de la tienda
 const crearTienda = () =>{
     let urlApi = 'v1/config/step1';
 
@@ -93,247 +97,10 @@ const crearTienda = () =>{
         });
 };
 
-const categoriaResumen = document.getElementById("categoriaResumen");
-const resumenContent = Array.from(document.querySelectorAll(".categoria__resumen--content"));
-
-categoriaResumen.addEventListener('click', (e) => {
-    let numeroPestaña = e.target.dataset.numero;
-    let pestaña = e.target.classList.contains('pestaña');
-
-    if (pestaña) {
-    pestañaSelec(numeroPestaña);
-    return;
-    }
-});
-
-const pestañaSelec = (numPestaña) => {
-    resumenContent.forEach(element => {
-        element.classList.remove('block');
-        if (element.dataset.pestaña === numPestaña) {
-            element.classList.add('block');
-            const button = document.querySelector(`button[data-numero="${numPestaña}"]`);
-            button.classList.add('selec');
-        } else {
-            const button = document.querySelector(`button[data-numero="${element.dataset.pestaña}"]`);
-            button.classList.remove('selec');
-        }
-    });
-};
-
-const inputContainers = document.querySelectorAll('.input-container');
-
-inputContainers.forEach((container) => {
-    const input = container.querySelector('.config__form--input');
-    const label = container.querySelector('.input-label');
-
-    input.addEventListener('focus', () => {
-        label.classList.add('active');
-        container.classList.remove('placeholder-visible');
-    });
-
-    input.addEventListener('blur', () => {
-        if (input.value === '') {
-            label.classList.remove('active');
-            container.classList.add('placeholder-visible');
-        }
-    });
-});
-
 // window.onload = mostrarPasoActual;window.onload = () => {
 //     mostrarPasoActual();
 // };
 
-var arrCategorias = Array();
-
-arrCategorias.push({id: 1, descripcion: "Uñas"});
-arrCategorias.push({id: 2, descripcion: "Pestañas"});
-arrCategorias.push({id: 3, descripcion: "Peluqueria"});
-arrCategorias.push({id: 4, descripcion: "Tatoo"});
-
-// Obtén el elemento select
-var select = document.getElementById("Categoría");
-
-// Itera sobre el array de categorías y crea las opciones
-arrCategorias.forEach(function(categoria) {
-    var option = document.createElement("option");
-    option.value = categoria.descripcion.toLowerCase();
-    option.text = categoria.descripcion;
-    select.add(option);
-});
-
-/*
-arrServiciosCliente.push(
-    {
-        id_categoria: 3,
-        desc_categoria : "Pestañas",
-        arrServicios : Array( { id : 0, 
-                                titulo : "",
-                                descripcion : "",
-                                horario} 
-            )
-    }
-)
-*/
-
-// const horariosSemanales = [
-//     { id: 1, dia: 'lunes', nombre: 'Lunes', rangos: [] },
-//     { id: 2, dia: 'martes', nombre: 'Martes', rangos: [] },
-//     { id: 3, dia: 'miercoles', nombre: 'Miércoles', rangos: [] },
-//     { id: 4, dia: 'jueves', nombre: 'Jueves', rangos: [] },
-//     { id: 5, dia: 'viernes', nombre: 'Viernes', rangos: [] },
-//     { id: 6, dia: 'sabado', nombre: 'Sábado', rangos: [] },
-//     { id: 7, dia: 'domingo', nombre: 'Domingo', rangos: [] }
-// ];
-
-// const diasContainer = document.querySelector('.dias__container');
-// const dias = diasContainer.querySelectorAll('div');
-// const agendaContainer = document.querySelector('.agenda__container');
-// const tituloDia = document.querySelector('.agenda__container h3');
-// const nombresDias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-// const selectDesde = document.getElementById('select__hora--desde');
-// const selectMinutoDesde = document.getElementById('select__minuto--desde');
-// const selectHasta = document.getElementById('select__hora--hasta');
-// const selectMinutoHasta = document.getElementById('select__minuto--hasta');
-// const btnAgregar = document.getElementById('btnAgregar');
-// const resumenContainer = document.querySelector('.agenda__resumen--container');
-
-// let rangoEditado = null;
-
-// const resetSelectOptions = () => {
-//     selectDesde.selectedIndex = 0;
-//     selectMinutoDesde.selectedIndex = 0;
-//     selectHasta.selectedIndex = 0;
-//     selectMinutoHasta.selectedIndex = 0;
-// };
-
-// const cargarDatosRango = (rango) => {
-//     const [desdeHora, desdeMinuto] = rango.desde.split(':');
-//     const [hastaHora, hastaMinuto] = rango.hasta.split(':');
-
-//     selectDesde.value = desdeHora;
-//     selectMinutoDesde.value = desdeMinuto;
-//     selectHasta.value = hastaHora;
-//     selectMinutoHasta.value = hastaMinuto;
-
-//     const diaId = rango.diaId - 1;
-//     seleccionarDia(dias[diaId], diaId);
-// };
-
-// const agregarRango = () => {
-//     const diaSeleccionadoElement = document.querySelector('.dias__container .select');
-//     const diaSeleccionado = diaSeleccionadoElement.querySelector('p').textContent;
-//     const diaId = parseInt(diaSeleccionadoElement.getAttribute('data-id'));
-
-//     const desde = selectDesde.value + ':' + selectMinutoDesde.value;
-//     const hasta = selectHasta.value + ':' + selectMinutoHasta.value;
-
-//     const dia = horariosSemanales.find((horario) => horario.dia === diaSeleccionado);
-//     if (dia) {
-//         dia.rangos.push({ desde, hasta, diaId });
-//     } else {
-//         const nuevoHorario = {
-//             id: diaId,
-//             dia: diaSeleccionado,
-//             rangos: [{ desde, hasta, diaId }]
-//         };
-//         horariosSemanales.push(nuevoHorario);
-//     }
-
-//     actualizarResumenHorarios();
-//     resetSelectOptions();
-// };
-
-// const editarRango = () => {
-//     if (rangoEditado) {
-//         rangoEditado.desde = selectDesde.value + ':' + selectMinutoDesde.value;
-//         rangoEditado.hasta = selectHasta.value + ':' + selectMinutoHasta.value;
-//         const diaSeleccionadoElement = document.querySelector('.dias__container .select');
-//         const diaSeleccionadoIndex = Array.from(dias).indexOf(diaSeleccionadoElement);
-//         rangoEditado.diaId = diaSeleccionadoIndex + 1;
-//         rangoEditado = null;
-
-//         seleccionarDia(dias[diaSeleccionadoIndex], diaSeleccionadoIndex);
-
-//         actualizarResumenHorarios();
-//         resetSelectOptions();
-//         btnAgregar.textContent = 'Agregar';
-//         btnAgregar.removeEventListener('click', editarRango);
-//         btnAgregar.addEventListener('click', agregarRango);
-//     };
-// };
-
-
-// const actualizarResumenHorarios = () => {
-//     resumenContainer.innerHTML = '';
-//     const diasOrdenados = horariosSemanales.sort((a, b) => a.id - b.id);
-
-//     diasOrdenados.forEach((horario) => {
-//         if (horario.rangos.length > 0) {
-//             const lista = document.createElement('div');
-//             lista.classList.add('agenda__resumen--lista');
-
-//             const h4 = document.createElement('h4');
-//             h4.textContent = nombresDias[horario.id - 1];
-
-//             const rangosContainer = document.createElement('div');
-//             rangosContainer.classList.add('agenda__resumen--rangos');
-
-//             horario.rangos.forEach((rango) => {
-//                 const p = document.createElement('p');
-//                 p.textContent = `Desde las ${rango.desde}, hasta las ${rango.hasta}.`;
-
-//                 const icono = document.createElement('i');
-//                 icono.classList.add('fa', 'fa-pencil');
-//                 icono.setAttribute('aria-hidden', 'true');
-//                 icono.addEventListener('click', () => {
-//                     cargarDatosRango(rango);
-//                     btnAgregar.textContent = 'Actualizar';
-//                     btnAgregar.removeEventListener('click', agregarRango);
-//                     btnAgregar.addEventListener('click', editarRango);
-//                     rangoEditado = rango;
-//                     const diaIndex = rango.diaId - 1;
-//                     seleccionarDia(dias[diaIndex], diaIndex);
-//                 });
-
-//                 p.appendChild(icono);
-//                 rangosContainer.appendChild(p);
-//             });
-
-//             lista.appendChild(h4);
-//             lista.appendChild(rangosContainer);
-//             resumenContainer.appendChild(lista);
-//         }
-//     });
-// };
-
-
-// const seleccionarDia = (diaElement, diaIndex) => {
-//     dias.forEach((div) => div.classList.remove('select'));
-//     diaElement.classList.add('select');
-//     const leftValue = diaElement.getAttribute('data-left');
-//     agendaContainer.style.setProperty('--left-value', leftValue + '%');
-
-//     const nombreDia = nombresDias[diaIndex];
-//     tituloDia.textContent = nombreDia;
-// };
-
-// dias.forEach((div, index) => {
-//     div.addEventListener('click', () => {
-//         seleccionarDia(div, index);
-
-//         resetSelectOptions();
-//         rangoEditado = null;
-//         btnAgregar.textContent = 'Agregar';
-//         btnAgregar.removeEventListener('click', editarRango);
-//         btnAgregar.addEventListener('click', agregarRango);
-//     });
-// });
-
-// btnAgregar.addEventListener('click', agregarRango);
-
-// actualizarResumenHorarios();
-
-// !! Hasta aca se pueden cargar los rangos horarios y se pueden editar, funcionan bien los titulos y la flecha que indica el dia 
 
 const horariosSemanales = [
     { id: 1, dia: 'lunes', nombre: 'Lunes', rangos: [] },
@@ -345,17 +112,17 @@ const horariosSemanales = [
     { id: 7, dia: 'domingo', nombre: 'Domingo', rangos: [] }
 ];
 
-const diasContainer = document.querySelector('.dias__container');
+const diasContainer = document.querySelector('.dias');
 const dias = diasContainer.querySelectorAll('div');
-const agendaContainer = document.querySelector('.agenda__container');
-const tituloDia = document.querySelector('.agenda__container h3');
+const agendaContainer = document.querySelector('.agenda');
+const tituloDia = document.querySelector('.agenda h3');
 const nombresDias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
 const selectDesde = document.getElementById('select__hora--desde');
 const selectMinutoDesde = document.getElementById('select__minuto--desde');
 const selectHasta = document.getElementById('select__hora--hasta');
 const selectMinutoHasta = document.getElementById('select__minuto--hasta');
 const btnAgregar = document.getElementById('btnAgregar');
-const resumenContainer = document.querySelector('.agenda__resumen--container');
+const resumenContainer = document.querySelector('.resumen');
 
 let rangoEditado = null;
 
@@ -380,7 +147,7 @@ const cargarDatosRango = (rango) => {
 };
 
 const agregarRango = () => {
-    const diaSeleccionadoElement = document.querySelector('.dias__container .select');
+    const diaSeleccionadoElement = document.querySelector('.dias .dias__select');
     const diaSeleccionadoIndex = Array.from(dias).indexOf(diaSeleccionadoElement);
     const diaSeleccionado = horariosSemanales.find((dia) => dia.id === diaSeleccionadoIndex + 1);
 
@@ -397,7 +164,7 @@ const editarRango = () => {
     if (rangoEditado) {
         rangoEditado.desde = selectDesde.value + ':' + selectMinutoDesde.value;
         rangoEditado.hasta = selectHasta.value + ':' + selectMinutoHasta.value;
-        const diaSeleccionadoElement = document.querySelector('.dias__container .select');
+        const diaSeleccionadoElement = document.querySelector('.dias .dias__select');
         const diaSeleccionadoIndex = Array.from(dias).indexOf(diaSeleccionadoElement);
         rangoEditado.diaId = diaSeleccionadoIndex + 1;
         rangoEditado = null;
@@ -413,7 +180,7 @@ const editarRango = () => {
 };
 
 const eliminarRango = (diaId, rango) => {
-    const diaSeleccionadoElement = document.querySelector('.dias__container .select');
+    const diaSeleccionadoElement = document.querySelector('.dias .dias__select');
     const diaSeleccionadoIndex = Array.from(dias).indexOf(diaSeleccionadoElement);
     const diaSeleccionado = horariosSemanales.find((dia) => dia.id === diaId);
 
@@ -434,20 +201,21 @@ const actualizarResumenHorarios = () => {
         //Recorre el arreglo del día de la semana correspondiente
         if (horario.rangos.length > 0) {
             const lista = document.createElement('div');
-            lista.classList.add('agenda__resumen--lista');
+            lista.classList.add('resumen__lista');
 
             const h4 = document.createElement('h4');
             h4.textContent = nombresDias[horario.id - 1];
 
             const rangosContainer = document.createElement('div');
-            rangosContainer.classList.add('agenda__resumen--rangos');
+            rangosContainer.classList.add('resumen__rangos');
 
             horario.rangos.forEach((rango) => {
                 const p = document.createElement('p');
+                p.classList.add('resumen__texto')
                 p.textContent = `Desde las ${rango.desde}, hasta las ${rango.hasta}.`;
 
                 const iconoEditar = document.createElement('i');
-                iconoEditar.classList.add('fa', 'fa-pencil');
+                iconoEditar.classList.add('fa', 'fa-pencil', 'resumen__icono');
                 iconoEditar.setAttribute('aria-hidden', 'true');
                 iconoEditar.addEventListener('click', () => {
                     cargarDatosRango(rango);
@@ -460,7 +228,7 @@ const actualizarResumenHorarios = () => {
                 });
 
                 const iconoEliminar = document.createElement('i');
-                iconoEliminar.classList.add('fa', 'fa-times');
+                iconoEliminar.classList.add('fa', 'fa-times', 'resumen__icono');
                 iconoEliminar.setAttribute('aria-hidden', 'true');
                 iconoEliminar.addEventListener('click', () => {
                     showAlert(
@@ -490,8 +258,8 @@ const actualizarResumenHorarios = () => {
 };
 
 const seleccionarDia = (diaElement, diaIndex) => {
-    dias.forEach((div) => div.classList.remove('select'));
-    diaElement.classList.add('select');
+    dias.forEach((div) => div.classList.remove('dias__select'));
+    diaElement.classList.add('dias__select');
     const leftValue = diaElement.getAttribute('data-left');
     agendaContainer.style.setProperty('--left-value', leftValue + '%');
 
@@ -515,4 +283,101 @@ btnAgregar.addEventListener('click', agregarRango);
 
 actualizarResumenHorarios();
 
-console.log(arrCategorias)
+
+const arrCategorias = Array();
+const arrServiciosCliente = Array()
+
+arrCategorias.push({id: 1, descripcion: "Uñas"});
+arrCategorias.push({id: 2, descripcion: "Pestañas"});
+arrCategorias.push({id: 3, descripcion: "Peluqueria"});
+arrCategorias.push({id: 4, descripcion: "Tatoo"});
+
+// Obtiene el elemento select
+const select = document.getElementById("categoría");
+
+// Itera sobre el array de categorías y crea las opciones
+arrCategorias.forEach((categoria) => {
+    var option = document.createElement("option");
+    option.value = categoria.descripcion.toLowerCase();
+    option.text = categoria.descripcion;
+    option.dataset.idCategoria = categoria.id;
+    select.add(option);
+});
+
+// Obtiene las pestañas del resumen y forma un array con el contenido
+const tabsCategoria = document.getElementById("tabsCategoria");
+const resumenContent = Array.from(document.querySelectorAll(".resumen__content"));
+// Obtiene el numero de pestaña seleccionada y ejecuta la funcion (pestañaSelec) pasandole ese numero como paramentro
+tabsCategoria.addEventListener('click', (e) => {
+    let numeroPestaña = e.target.dataset.numero;
+    let pestaña = e.target.classList.contains('resumen__tabsBtn');
+
+    if (pestaña) {
+    pestañaSelec(numeroPestaña);
+    return;
+    }
+});
+// Iterra sobre el array del contenido y va mostrando solo lo que corresponde al numero de pestaña seleccionada
+const pestañaSelec = (numPestaña) => {
+    resumenContent.forEach(element => {
+        element.classList.remove('block');
+        if (element.dataset.pestaña === numPestaña) {
+            element.classList.add('block');
+            const button = document.querySelector(`button[data-numero="${numPestaña}"]`);
+            button.classList.add('tabSelect');
+        } else {
+            const button = document.querySelector(`button[data-numero="${element.dataset.pestaña}"]`);
+            button.classList.remove('tabSelect');
+        }
+    });
+};
+
+let btnCargar = document.getElementById("btnCargar");
+
+// Agregar evento al botón "Cargar"
+btnCargar.addEventListener("click", ()=> {
+    // Obtener los valores de los campos de entrada
+    let servicioTitulo = document.getElementById("servicio").value;
+    let descripcion = document.getElementById("servDesc").value;
+    let tiempoHoras = document.getElementById("tiempoHoras").value;
+    let tiempoMinutos = document.getElementById("tiempoMinutos").value;
+    let servPrecio = document.getElementById("servPrecio").value;
+
+//   Crear el objeto con los valores capturados
+    let servicio = {
+        id: 0,
+        id_categoria: 99,
+        titulo:servicioTitulo,
+        descripcion: descripcion,
+        tiempo: tiempoHoras + ":" + tiempoMinutos,
+        precio: servPrecio
+    };
+
+    console.log(servicio);
+
+//////////////////////////////////////////////////////////////
+    servicio.id = Math.random();
+//////////////////////////////////////////////////////////////////
+
+
+
+
+
+});
+
+
+/*
+arrServiciosCliente.push(
+    {
+        id_categoria: 3,
+        desc_categoria : "Pestañas",
+        arrServicios : Array( { id : 0, 
+                                titulo : "",
+                                descripcion : "",
+                                horario} 
+            )
+    }
+)
+*/
+
+
